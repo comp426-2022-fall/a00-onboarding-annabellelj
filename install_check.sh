f8efcdd4f0b7c47fac22e159689fa09d1c349646
Annabelle Jiang <69731314+annabellelj@users.noreply.github.com>
2022-08-21 13:29:12 -0400

    Delete install_check.sh
    Signed-off-by: Annabelle Jiang <69731314+annabellelj@users.noreply.github.com>

--- install_check.sh
+++
@@ -1,8 +0,0 @@
-Install checked: 2022-08-18T02:49:16-04:00
-
-SYSTEM: Linux user 5.18.0-0.bpo.1-amd64 #1 SMP PREEMPT_DYNAMIC Debian 5.18.2-1~bpo11+1 (2022-06-14) x86_64 GNU/Linux
-SHELL:  [/bin/bash]
-GIT:    [/usr/bin/git] git version 2.30.2
-VIM:    [/usr/bin/vim] VIM - Vi IMproved 8.2 (2019 Dec 12, compiled Oct 01 2021 01:51:08)
-CURL:   [/usr/bin/curl] curl 7.74.0 (x86_64-pc-linux-gnu) libcurl/7.74.0 OpenSSL/1.1.1n zlib/1.2.11 brotli/1.0.9 libidn2/2.3.0 libpsl/0.21.0 (+libidn2/2.3.0) libssh2/1.9.0 nghttp2/1.43.0 librtmp/2.3
-NODE:   [/home/user/.nvm/versions/node/v16.17.0/bin/node] v16.17.0
